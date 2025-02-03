a9de0f9b224d32cdef1b3b0aef6a225150887516
Federico Builes <federico.builes@gmail.com>
2025-02-03 14:40:09 +0100

    Adding longestloop.go

---
+++ longestloop/longestloop.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func longestLoop(s string) string {
+
+	coords := make(map[[2]int]int)
+	coords[[2]int{0, 0}] = -1
+
+	x, y := 0, 0
+	longest := 0
+	res := ""
+	for i, ch := range s {
+
+		switch ch {
+		case 'u':
+			y++
+		case 'd':
+			y--
+		case 'l':
+			x--
+		case 'r':
+			x++
+		}
+		if prevIndex, exists := coords[[2]int{x, y}]; exists {
+			length := i - prevIndex
+			if length > longest {
+				longest = length
+				res = s[prevIndex+1 : i+1]
+			}
+		} else {
+			coords[[2]int{x, y}] = i
+		}
+
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(longestLoop("du"))
+}
