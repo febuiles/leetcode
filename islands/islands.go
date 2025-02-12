a8aa0b618cb0c50c255043b6534a9bc52805b0f6
Federico Builes <federico.builes@gmail.com>
2025-02-12 16:16:23 +0100

    use dfs

--- islands/islands.go
+++ islands/islands.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func numIslands(grid [][]byte) int {
+	rows := len(grid)
+	cols := len(grid[0])
 	numIslands := 0
 
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[i]); j++ {
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
 			if grid[i][j] == '1' {
-				if numIslands == 0 || !nearbyIsland(&grid, i, j) {
-					numIslands++
-				}
+				numIslands++
+				dfs(&grid, i, j)
 			}
 		}
 	}
@@ -18,27 +19,36 @@ func numIslands(grid [][]byte) int {
 	return numIslands
 }
 
-func nearbyIsland(grid *[][]byte, i int, j int) bool {
-	if i-1 >= 0 {
-		if (*grid)[i-1][j] == '1' {
-			return true
-		}
+func dfs(grid *[][]byte, i, j int) {
+	rows := len(*grid)
+	cols := len((*grid)[0])
+
+	(*grid)[i][j] = '0'
+
+	if i-1 >= 0 && (*grid)[i-1][j] == '1' {
+		dfs(grid, i-1, j)
 	}
 
-	if j-1 >= 0 {
-		if (*grid)[i][j-1] == '1' {
-			return true
-		}
+	if i+1 < rows && (*grid)[i+1][j] == '1' {
+		dfs(grid, i+1, j)
 	}
 
-	return false
+	if j-1 >= 0 && (*grid)[i][j-1] == '1' {
+		dfs(grid, i, j-1)
+	}
+
+	if j+1 < cols && (*grid)[i][j+1] == '1' {
+		dfs(grid, i, j+1)
+	}
 }
 
 func main() {
+
 	input := [][]byte{
-		{'1', '0', '1', '1', '1'},
-		{'1', '0', '1', '0', '1'},
-		{'1', '1', '1', '0', '1'}}
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'}}
 
 	fmt.Println(numIslands(input))
 }
